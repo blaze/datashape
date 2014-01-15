99be8919a0bc274dc311ebe3201dfc490a1d0d07
Mark Wiebe <mwiebe@continuum.io>
2014-01-15 12:38:01 -0800

    Remove find_packages import, it's not in distutils

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup, find_packages
+from distutils.core import setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -17,7 +17,7 @@ setup(
     license = "BSD",
     keywords = "data language",
     url = "http://packages.python.org/datashape",
-    packages = ["datashape", "datashape.test"],
+    packages = ["datashape", "datashape.tests"],
     long_description = read('README.md'),
     classifiers = [
         "Development Status :: 3 - Alpha",
