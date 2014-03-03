6d25061641c137d1dee412970a35048f14365162
Mark Wiebe <mwiebe@continuum.io>
2014-03-03 01:49:06 -0800

    Adjust equation structure to directly match the matching 'dst'

--- datashape/tests/test_type_equation_solver.py
+++ datashape/tests/test_type_equation_solver.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import unittest
 
 from datashape import coretypes as T
-from datashape.type_equation_solver import match_argtypes_to_signature, explode_coercion_eqns
+from datashape.type_equation_solver import match_argtypes_to_signature, _match_equation
 from datashape import dshape
 
 
@@ -19,63 +19,61 @@ class TestCoercion(unittest.TestCase):
 
     def test_explode_coercion_eqns_dtype(self):
         # A simple coercion
-        beqn, deqn = explode_coercion_eqns([(dshape('int32'), dshape('int64'))])
-        self.assertEqual(beqn, [])
-        self.assertEqual(deqn, [(T.int32, T.int64, 0)])
+        eqns = _match_equation(dshape('int32'), dshape('int64'))
+        self.assertEqual(eqns, [(T.int32, T.int64)])
         # Matching a data type variable
-        beqn, deqn = explode_coercion_eqns([(dshape('int32'), dshape('D'))])
-        self.assertEqual(beqn, [])
-        self.assertEqual(deqn, [(T.int32, T.TypeVar('D'), 0)])
+        eqns = _match_equation(dshape('int32'), dshape('D'))
+        self.assertEqual(eqns, [(T.int32, T.TypeVar('D'))])
 
     def test_explode_coercion_eqns_dim(self):
         # Broadcasting a single dimension
-        beqn, deqn = explode_coercion_eqns([(dshape('1 * int32'), dshape('10 * int32'))])
-        self.assertEqual(beqn, [(T.Fixed(1), T.Fixed(10), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('1 * int32'), dshape('10 * int32'))
+        self.assertEqual(eqns, [(T.Fixed(1), T.Fixed(10)),
+                                (T.int32, T.int32)])
         # Matching a dim type variable
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * int32'), dshape('M * int32'))])
-        self.assertEqual(beqn, [(T.Fixed(3), T.TypeVar('M'), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('3 * int32'), dshape('M * int32'))
+        self.assertEqual(eqns, [(T.Fixed(3), T.TypeVar('M')),
+                                (T.int32, T.int32)])
 
     def test_explode_coercion_eqns_ellipsis(self):
         # Matching an ellipsis
-        beqn, deqn = explode_coercion_eqns([(dshape('int32'), dshape('... * int32'))])
-        self.assertEqual(beqn, [([], T.Ellipsis(), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * int32'), dshape('... * int32'))])
-        self.assertEqual(beqn, [([T.Fixed(3)], T.Ellipsis(), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * int32'), dshape('... * int32'))])
-        self.assertEqual(beqn, [([T.Fixed(3), T.Var()], T.Ellipsis(), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('int32'), dshape('... * int32'))
+        self.assertEqual(eqns, [([], T.Ellipsis()),
+                                (T.int32, T.int32)])
+        eqns = _match_equation(dshape('3 * int32'), dshape('... * int32'))
+        self.assertEqual(eqns, [([T.Fixed(3)], T.Ellipsis()),
+                                (T.int32, T.int32)])
+        eqns = _match_equation(dshape('3 * var * int32'), dshape('... * int32'))
+        self.assertEqual(eqns, [([T.Fixed(3), T.Var()], T.Ellipsis()),
+                                (T.int32, T.int32)])
         # Matching an ellipsis type variable
-        beqn, deqn = explode_coercion_eqns([(dshape('int32'), dshape('A... * int32'))])
-        self.assertEqual(beqn, [([], T.Ellipsis(T.TypeVar('A')), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * int32'), dshape('A... * int32'))])
-        self.assertEqual(beqn, [([T.Fixed(3)], T.Ellipsis(T.TypeVar('A')), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * int32'), dshape('A... * int32'))])
-        self.assertEqual(beqn, [([T.Fixed(3), T.Var()], T.Ellipsis(T.TypeVar('A')), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('int32'), dshape('A... * int32'))
+        self.assertEqual(eqns, [([], T.Ellipsis(T.TypeVar('A'))),
+                                (T.int32, T.int32)])
+        eqns = _match_equation(dshape('3 * int32'), dshape('A... * int32'))
+        self.assertEqual(eqns, [([T.Fixed(3)], T.Ellipsis(T.TypeVar('A'))),
+                                (T.int32, T.int32)])
+        eqns = _match_equation(dshape('3 * var * int32'), dshape('A... * int32'))
+        self.assertEqual(eqns, [([T.Fixed(3), T.Var()], T.Ellipsis(T.TypeVar('A'))),
+                                (T.int32, T.int32)])
         # Matching an ellipsis with a dim type variable on the left
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * int32'), dshape('A * B... * int32'))])
-        self.assertEqual(beqn, [(T.Fixed(3), T.TypeVar('A'), 0),
-                                ([T.Var()], T.Ellipsis(T.TypeVar('B')), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('3 * var * int32'), dshape('A * B... * int32'))
+        self.assertEqual(eqns, [(T.Fixed(3), T.TypeVar('A')),
+                                ([T.Var()], T.Ellipsis(T.TypeVar('B'))),
+                                (T.int32, T.int32)])
         # Matching an ellipsis with a dim type variable on the right
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * int32'), dshape('A... * B * int32'))])
-        self.assertEqual(beqn, [([T.Fixed(3)], T.Ellipsis(T.TypeVar('A')), 0),
-                                (T.Var(), T.TypeVar('B'), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('3 * var * int32'), dshape('A... * B * int32'))
+        self.assertEqual(eqns, [([T.Fixed(3)], T.Ellipsis(T.TypeVar('A'))),
+                                (T.Var(), T.TypeVar('B')),
+                                (T.int32, T.int32)])
         # Matching an ellipsis with a dim type variable on both sides
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * int32'), dshape('A * B... * C * int32'))])
-        self.assertEqual(beqn, [(T.Fixed(3), T.TypeVar('A'), 0),
-                                ([], T.Ellipsis(T.TypeVar('B')), 0),
-                                (T.Var(), T.TypeVar('C'), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
-        beqn, deqn = explode_coercion_eqns([(dshape('3 * var * 4 * M * int32'), dshape('A * B... * C * int32'))])
-        self.assertEqual(beqn, [(T.Fixed(3), T.TypeVar('A'), 0),
-                                ([T.Var(), T.Fixed(4)], T.Ellipsis(T.TypeVar('B')), 0),
-                                (T.TypeVar('M'), T.TypeVar('C'), 0)])
-        self.assertEqual(deqn, [(T.int32, T.int32, 0)])
+        eqns = _match_equation(dshape('3 * var * int32'), dshape('A * B... * C * int32'))
+        self.assertEqual(eqns, [(T.Fixed(3), T.TypeVar('A')),
+                                ([], T.Ellipsis(T.TypeVar('B'))),
+                                (T.Var(), T.TypeVar('C')),
+                                (T.int32, T.int32)])
+        eqns = _match_equation(dshape('3 * var * 4 * M * int32'), dshape('A * B... * C * int32'))
+        self.assertEqual(eqns, [(T.Fixed(3), T.TypeVar('A')),
+                                ([T.Var(), T.Fixed(4)], T.Ellipsis(T.TypeVar('B'))),
+                                (T.TypeVar('M'), T.TypeVar('C')),
+                                (T.int32, T.int32)])
--- datashape/type_equation_solver.py
+++ datashape/type_equation_solver.py
@@ -9,9 +9,16 @@ __all__ = ['match_argtypes_to_signature', 'explode_coercion_eqns']
 
 from . import coretypes
 from . import error
+from . import coercion
+
+inf = float('inf')
 
 
-def match_argtypes_to_signature(argtypes, signature):
+class _PruneProcessing(Exception):
+    """An exception thrown when a possible match is pruned because its cost is higher than the threshold."""
+
+
+def match_argtypes_to_signature(argtypes, signature, cutoff_cost):
     """
     Performs a pattern matching of the argument types against the
     function signature. Raises an exception if it cannot be matched,
@@ -24,6 +31,7 @@ def match_argtypes_to_signature(argtypes, signature):
     signature : Function signature datashape object
         A datashape function signature type against which to match.
     """
+    # Pull the Tuple and Function out of the DataShape wrappers
     if isinstance(argtypes, coretypes.DataShape) and len(argtypes) == 1:
         argtypes = argtypes[0]
     else:
@@ -32,10 +40,12 @@ def match_argtypes_to_signature(argtypes, signature):
         signature = signature[0]
     else:
         raise TypeError('invalid argtypes %s' % argtypes)
+    # Validate the argument types
     if not isinstance(argtypes, coretypes.Tuple):
         raise TypeError('argtypes must be a datashape.Tuple')
     if not isinstance(signature, coretypes.Function):
         raise TypeError('signature must be a datashape.Function')
+
     # The number of arguments must match
     if len(argtypes.dshapes) != len(signature.argtypes):
         raise TypeError(('Cannot match signature, expected ' +
@@ -43,54 +53,129 @@ def match_argtypes_to_signature(argtypes, signature):
                         (len(signature.argtypes), len(argtypes)))
     # First build a system of coercion equations
     eqns = zip(argtypes.dshapes, signature.argtypes)
-    # Break it down into a system of separate broadcasting and coercion equations
-    bcast_eqns, coerce_eqns = explode_coercion_eqns(eqns)
+    # Break down each equation down into a series of equations
+    # with the same structure as the 'dst' datashape
+    eqns = [_match_equation(src, dst) for src, dst in eqns]
+    dim_tv, dtype_tv = {}
+
+    # Ensure that no TypeVar symbol has been used in multiple ways
+    for tv in bcast_eqns:
+        if isinstance(tv, coretypes.Ellipsis):
+            s = tv.typevar
+            if s in bcast_eqns:
+                raise TypeError(('DataShape typevar %s has been ' +
+                                 'used as both a dim and an ellipsis') % (s))
+            elif s in coerce_eqns:
+                raise TypeError(('DataShape typevar %s has been ' +
+                                 'used as both a dtype and an ellipsis') % (s))
+        else:
+            if s in coerce_eqns:
+                raise TypeError(('DataShape typevar %s has been ' +
+                                 'used as both a dtype and a dim') % (s))
+    # Do all the promotions of the
 
 
-def explode_coercion_eqns(eqns):
+def _match_equation(src, dst):
     """
-    Breaks a system of datashape coercion equations into simpler
-    dimension type broadcast equations and data type coercion equations.
+    Matches a single src datashape against a dst datashape, building
+    a nested structure of equations matching the individual terms
+    in 'dst' to the corresponding parts in 'src'.
 
-    The resulting equations are in the form (src, dst, equation_index),
-    including the index where they came from to help with substituting
-    values for type variables later.
+    As part of the matching, values taken on by type variables are
+    accumulated in dim_tv and dtype_tv.
     """
-    bcast_eqns = []
-    coerce_eqns = []
-    for eqn_idx, (src_ds, dst_ds) in enumerate(eqns):
-        # Add the data type coercion
-        coerce_eqns.append((src_ds[-1], dst_ds[-1], eqn_idx))
-        # Add all the broadcasting operations, starting from the right
+    if isinstance(src, coretypes.DataShape) and isinstance(dst, coretypes.DataShape):
+        # Start off with a list matching the length of 'dst'
+        eqns = [None] * len(dst)
+        # Add all the broadcasting operations
         src_i, dst_i = 0, 0
-        src_j, dst_j = len(src_ds) - 2, len(dst_ds) - 2
-        while src_j >= src_i and dst_j >= dst_i:
-            src = src_ds[src_j]
-            dst = dst_ds[dst_j]
-            if isinstance(dst, coretypes.Ellipsis):
-                # Since we hit an ellipsis, we need to now process
-                # the dims from the left to drill down on the part
-                # which broadcasts via adding dimensions
-                pre_eqn = []
-                while src_i <= src_j and dst_i < dst_j:
-                    pre_eqn.append((src_ds[src_i], dst_ds[dst_i], eqn_idx))
-                    src_i, dst_i = src_i + 1, dst_i + 1
-                # When broadcasting against an ellipsis, the src side gets a
-                # list of dimensions, instead of just one
-                src = [src_ds[i] for i in range(src_i, src_j + 1)]
-                pre_eqn.append((src, dst, eqn_idx))
-                pre_eqn.extend(bcast_eqns)
-                bcast_eqns = pre_eqn
-                src_j, dst_j = src_i - 1, dst_j - 1
+        src_j, dst_j = len(src) - 2, len(dst) - 2
+        # First match from the left, until we hit an ellipsis
+        while src_i <= src_j and dst_i <= dst_j:
+            if not isinstance(dst[dst_i], coretypes.Ellipsis):
+                eqns[dst_i] = (src[src_i], dst[dst_i])
+                src_i, dst_i = src_i + 1, dst_i + 1
             else:
-                bcast_eqns.insert(0, (src, dst, eqn_idx))
+                break
+        # Then match from the right, until we hit an ellipsis
+        while src_i <= src_j and dst_i <= dst_j:
+            if not isinstance(dst[dst_j], coretypes.Ellipsis):
+                eqns[dst_j] = (src[src_j], dst[dst_j])
                 src_j, dst_j = src_j - 1, dst_j - 1
-        # As a special case, an ellipsis can match against nothing
-        if src_i > src_j and dst_i == dst_j and isinstance(dst_ds[dst_i], coretypes.Ellipsis):
-            bcast_eqns.insert(0, ([], dst_ds[dst_j], eqn_idx))
-            dst_j = dst_j - 1
+            else:
+                break
+        # Handle the ellipsis, if we hit one
+        if dst_i == dst_j and isinstance(dst[dst_i], coretypes.Ellipsis):
+            eqns[dst_i] = ([src[i] for i in range(src_i, src_j + 1)],
+                           dst[dst_i])
+            src_i, dst_i = src_j + 1, dst_i + 1
         # If we didn't match all the dimensions together, it's an error
         if src_i <= src_j or dst_i <= dst_j:
-            raise error.UnificationError(('Could not coerce dimensions of ' +
-                                          '%s into %s') % (src_ds, dst_ds))
-    return (bcast_eqns, coerce_eqns)
+            raise error.CoercionError(src, dst)
+        # Match the data type
+        # TODO: For struct and similar types, make a recursive call here
+        eqns[-1] = (src[-1], dst[-1])
+        return eqns
+    else:
+        raise TypeError(('Only DataShape matching is implemented, ' +
+                         'not yet %s or %s') % (src, dst))
+
+
+def process_broadcast_equations(bcast_eqns):
+    """
+    Collects all of the type variable values into a dictionary,
+    and returns it with the maximum broadcasting cost.
+    """
+    vals = {}
+    cost = 0
+    for src, dst, eqn_idx in bcast_eqns:
+        if isinstance(dst, coretypes.Ellipsis):
+            # Indicate that the src value must be broadcastable to the type var value
+            # TODO: If there is an "exact" version of the ellipsis typevar added, this would be '==' for it
+            v = vals.get(dst, [])
+            v.append(('>=', src, eqn_idx))
+            vals[dst] = v
+        elif isinstance(dst, coretypes.TypeVar):
+            # Indicate that the src value must be equal to the type var value
+            # TODO: If there is an "inexact" version of the typevar added, this would be '>=' for it
+            v = vals.get(dst, [])
+            v.append(('==', src, eqn_idx))
+            vals[dst] = v
+        elif isinstance(src, coretypes.TypeVar):
+            # Indicate that the type var value must be equal to the dst value
+            v = vals.get(src, [])
+            v.append(('==', dst, eqn_idx))
+            vals[src] = v
+        else:
+            # This is a concrete broadcasting, evaluate its cost
+            c = coercion.dim_coercion_cost(src, dst)
+            if c == inf:
+                raise error.CoercionError(src, dst)
+            cost = max(cost, c)
+    return (vals, cost)
+
+def process_coercion_equations(coerce_eqns):
+    """
+    Collects all of the type variable values into a dictionary,
+    and returns the maximum coercion cost.
+    """
+    vals = {}
+    cost = 0
+    for src, dst, eqn_idx in coerce_eqns:
+        if isinstance(dst, coretypes.TypeVar):
+            # Indicate that the src value must be coercible to the type var value
+            # TODO: If there is an "exact" version of the typevar added, this would be '==' for it
+            v = vals.get(dst, [])
+            v.append(('>=', src, eqn_idx))
+            vals[dst] = v
+        elif isinstance(src, coretypes.TypeVar):
+            v = vals.get(src, [])
+            v.append(('<=', dst, eqn_idx))
+            vals[src] = v
+        else:
+            # This is a concrete coerciion, evaluate its cost
+            c = coercion.dtype_coercion_cost(src, dst)
+            if c == inf:
+                raise error.CoercionError(src, dst)
+            cost = max(cost, c)
+    return (vals, cost)
