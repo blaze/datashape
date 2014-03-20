7f7d5c0873a703f6ee7c9dd0faed673763770ddb
Mark Wiebe <mwiebe@continuum.io>
2014-03-19 20:39:13 -0700

    Expose OverloadResolver at datashape level

--- datashape/__init__.py
+++ datashape/__init__.py
@@ -5,6 +5,7 @@ from . import type_equation_solver
 from .coretypes import *
 from .typesets import *
 from .type_symbol_table import *
+from .overload_resolver import *
 from .util import *
 from .coercion import coercion_cost
 from .error import (DataShapeSyntaxError, OverloadError, UnificationError,
