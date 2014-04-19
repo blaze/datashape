ce6e1983acde3a4c52b028a6a87eeec24e8bfc9c
Matthew Rocklin <mrocklin@gmail.com>
2014-04-18 18:56:38 -0700

    add ishomogenous

--- datashape/predicates.py
+++ datashape/predicates.py
@@ -1,9 +1,11 @@
-from .util import collect
+from .util import collect, remove
 from .coretypes import *
 
 # https://github.com/ContinuumIO/datashape/blob/master/docs/source/types.rst
 
 dimension_types = (Fixed, Var, Ellipsis)
+
+isunit = lambda x: isinstance(x, Unit)
 
 def isdimension(ds):
     """ Is a component a dimension?
@@ -16,3 +18,16 @@ def isdimension(ds):
     False
     """
     return isinstance(ds, dimension_types)
+
+
+def ishomogenous(ds):
+    """ Does datashape contain only one dtype?
+
+    >>> ishomogenous(int32)
+    True
+    >>> ishomogenous(var * (3 * string))
+    True
+    >>> ishomogenous(var * Record([('name', string), ('amount', int32)]))
+    False
+    """
+    return len(set(remove(isdimension, collect(isunit, ds)))) == 1
