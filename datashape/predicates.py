96f66f47303ae13af89f03f48b309f067b7ce0b7
Matthew Rocklin <mrocklin@gmail.com>
2014-04-18 19:06:12 -0700

    number of dimensions function
    I'm not sure that this is how we want to define things

--- datashape/predicates.py
+++ datashape/predicates.py
@@ -1,4 +1,4 @@
-from .util import collect, remove
+from .util import collect, remove, dshape
 from .coretypes import *
 
 # https://github.com/ContinuumIO/datashape/blob/master/docs/source/types.rst
@@ -31,3 +31,28 @@ def ishomogenous(ds):
     False
     """
     return len(set(remove(isdimension, collect(isunit, ds)))) == 1
+
+
+def dimensions(ds):
+    """ Number of dimensions of datashape
+
+    Interprets records as dimensional
+
+    >>> dimensions(int32)
+    0
+    >>> dimensions(10 * int32)
+    1
+    >>> dimensions(var * (10 * int32))
+    2
+    >>> dimensions(var * Record([('name', string), ('amount', int32)]))
+    2
+    """
+    if not isinstance(ds, DataShape):
+        ds = dshape(ds)
+    if isdimension(ds[0]):
+        return 1 + dimensions(ds.subarray(1))
+    if isinstance(ds[0], Record):
+        return 1 + max(map(dimensions, ds[0].fields.values()))
+    if len(ds) == 1 and isunit(ds[0]):
+        return 0
+    raise NotImplementedError('Can not compute dimensions for %s' % ds)
