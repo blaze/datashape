dd1d7b295e7bd97bffe1b174c0e21c972b58870f
Matthew Rocklin <mrocklin@gmail.com>
2014-05-06 17:01:12 -0700

    add IndexCallable

--- datashape/util.py
+++ datashape/util.py
@@ -332,3 +332,21 @@ def from_ctypes(ctype):
 
 def remove(predicate, seq):
     return filter(lambda x: not predicate(x), seq)
+
+
+class IndexCallable(object):
+    """ Provide getitem syntax for functions
+
+    >>> def inc(x):
+    ...     return x + 1
+
+    >>> I = IndexCallable(inc)
+    >>> I[3]
+    4
+    """
+    __slots__ = 'fn',
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __getitem__(self, key):
+        return self.fn(key)
