17ff9bbf5d17fd8d6dc7612a76c06bedeb9344f7
Wesley Emeneker <Wesley.Emeneker@gmail.com>
2014-08-27 14:10:08 -0400

    Added tests to make sure that Record() rewrites special characters in field names.

--- datashape/tests/test_coretypes.py
+++ datashape/tests/test_coretypes.py
@@ -1,4 +1,4 @@
-from datashape.coretypes import Record, real
+from datashape.coretypes import Record, real, int64, datetime, float64
 from datashape import dshape, to_numpy_dtype
 import numpy as np
 import unittest
@@ -7,12 +7,25 @@ class TestRecord(unittest.TestCase):
     def setUp(self):
         self.a = Record([('x', int), ('y', int)])
         self.b = Record([('y', int), ('x', int)])
+        #Record.__init__ should escape all of the quotes, spaces, and other special characters, turning them into "_"s
+        self.c = Record([('Unique Key', int64), ('Creation Date', datetime)])
+        self.d = Record([('Unique Field(foo)', float64), ("Unique Field (foo2)", int)])
+        self.e = Record([('"Unique Field(foo)"', float64), ("'Unique Field (foo2)'", int)])
 
     def test_respects_order(self):
         self.assertNotEqual(self.a, self.b)
 
     def test_strings(self):
         self.assertEqual(Record([('x', 'real')]), Record([('x', real)]))
+
+    def test_name_normalization(self):
+        assert("Unique_Key" in self.c.names)
+        assert("Creation_Date" in self.c.names)
+        assert("Unique_Field_foo_" in self.d.names)
+        assert("Unique_Field__foo2_" in self.d.names)
+        assert("_Unique_Field_foo__" in self.e.names)
+        assert("_Unique_Field__foo2__" in self.e.names)
+                        
 
 class Test_to_numpy_dtype(unittest.TestCase):
     def test_simple(self):
