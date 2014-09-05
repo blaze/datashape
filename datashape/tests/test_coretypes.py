c8426c381a17cb89c164ebe53a5c6e3fb5fd5596
Wesley Emeneker <Wesley.Emeneker@gmail.com>
2014-09-05 09:37:48 -0400

    Updated record_string to correctly escape quotes and backslashes when turning the datashape into a string. This fixes a problem in blaze where converting from one datatype to another would throw errors when transferring the datashape from one source of data to another. Several tests have been added and all tests pass.

--- datashape/tests/test_coretypes.py
+++ datashape/tests/test_coretypes.py
@@ -2,6 +2,7 @@ import pickle
 
 import numpy as np
 import pytest
+import unittest
 
 from datashape.coretypes import (Record, real, String, CType, DataShape, int32,
         Fixed, Option)
@@ -115,3 +116,53 @@ def test_shape():
 
 def test_option_sanitizes_strings():
     assert Option('float32').ty == dshape('float32').measure
+
+
+class TestComplexFieldNames(unittest.TestCase):
+    """
+    The tests in this class should verify that the datashape parser can handle field names that contain
+      strange characters like spaces, quotes, and backslashes
+    The idea is that any given input datashape should be recoverable once we have created the actual dshape object.
+
+    This test suite is by no means complete, but it does handle some of the more common special cases (common special? oxymoron?)
+    """
+
+
+    space_dshape="""{ 'Unique Key' : ?int64 }"""
+
+    big_space_dshape="""{ 'Unique Key' : ?int64, 'Created Date' : string, 'Closed Date' : string, Agency : string, 'Agency Name' : string, 'Complaint Type' : string, Descriptor : string, 'Location Type' : string, 'Incident Zip' : ?int64, 'Incident Address' : ?string, 'Street Name' : ?string, 'Cross Street 1' : ?string, 'Cross Street 2' : ?string, 'Intersection Street 1' : ?string, 'Intersection Street 2' : ?string, 'Address Type' : string, City : string, Landmark : string, 'Facility Type' : string, Status : string, 'Due Date' : string, 'Resolution Action Updated Date' : string, 'Community Board' : string, Borough : string, 'X Coordinate (State Plane)' : ?int64, 'Y Coordinate (State Plane)' : ?int64, 'Park Facility Name' : string, 'Park Borough' : string, 'School Name' : string, 'School Number' : string, 'School Region' : string, 'School Code' : string, 'School Phone Number' : string, 'School Address' : string, 'School City' : string, 'School State' : string, 'School Zip' : string, 'School Not Found' : string, 'School or Citywide Complaint' : string, 'Vehicle Type' : string, 'Taxi Company Borough' : string, 'Taxi Pick Up Location' : string, 'Bridge Highway Name' : string, 'Bridge Highway Direction' : string, 'Road Ramp' : string, 'Bridge Highway Segment' : string, 'Garage Lot Name' : string, 'Ferry Direction' : string, 'Ferry Terminal Name' : string, Latitude : ?float64, Longitude : ?float64, Location : string }"""
+
+    bad_dshape="""{ Unique Key : int64}"""
+
+    quotes_dshape_01="""{ 'field \\' with \\' quotes' : string }"""
+    quotes_dshape_02="""{ 'doublequote \" field \"' : int64 }"""
+    quotes_dshape_03="""{ 'field \\' with \\' quotes' : string, 'doublequote \" field \"' : int64 }"""
+
+    backslash_dshape="""{ 'field with    backslashes' : int64 }"""
+
+    def test_spaces_01(self):
+        ds1=dshape(self.space_dshape)
+        self.assertEqual(self.space_dshape, str(ds1))
+
+    def test_spaces_02(self):
+        ds1=dshape(self.big_space_dshape)
+        self.assertEqual(self.big_space_dshape, str(ds1))
+
+    def test_quotes_01(self):
+        ds1=dshape(self.quotes_dshape_01)
+        self.assertEqual(self.quotes_dshape_01, str(ds1))
+
+    def test_quotes_02(self):
+        ds1=dshape(self.quotes_dshape_02)
+        self.assertEqual(self.quotes_dshape_02, str(ds1))
+
+    def test_quotes_03(self):
+        ds1=dshape(self.quotes_dshape_02)
+        self.assertEqual(self.quotes_dshape_02, str(ds1))
+
+    def test_backslash_01(self):
+        ds1=dshape(self.backslash_dshape)
+        self.assertEqual(self.backslash_dshape, str(ds1))
+
+    def test_bad_01(self):
+        self.assertRaises(Exception, dshape, self.bad_dshape)
