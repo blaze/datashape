f5d23c557c6126b3894d6f8741fe916681683ebf
Phillip Cloud <cpcloud@gmail.com>
2014-09-08 16:11:18 -0400

    Add from_numpy tests

--- datashape/tests/test_coretypes.py
+++ datashape/tests/test_coretypes.py
@@ -1,5 +1,5 @@
 from datashape.coretypes import Record, real
-from datashape import dshape, to_numpy_dtype
+from datashape import dshape, to_numpy_dtype, from_numpy
 import numpy as np
 import unittest
 
@@ -13,6 +13,7 @@ class TestRecord(unittest.TestCase):
 
     def test_strings(self):
         self.assertEqual(Record([('x', 'real')]), Record([('x', real)]))
+
 
 class Test_to_numpy_dtype(unittest.TestCase):
     def test_simple(self):
@@ -31,6 +32,37 @@ class Test_to_numpy_dtype(unittest.TestCase):
     def test_string(self):
         self.assertEqual(to_numpy_dtype(dshape('2 * string')),
                          np.dtype('O'))
+
+
+class TestFromNumPyDtype(object):
+
+    def test_int32(self):
+        assert from_numpy((2,), 'int32') == dshape('2 * int32')
+        assert from_numpy((2,), 'i4') == dshape('2 * int32')
+
+    def test_struct(self):
+        dtype = np.dtype([('x', '<i4'), ('y', '<i4')])
+        result = from_numpy((2,), dtype)
+        assert result == dshape('2 * {x: int32, y: int32}')
+
+    def test_datetime(self):
+        keys = 'h', 'm', 's', 'ms', 'us', 'ns', 'ps', 'fs', 'as'
+        for k in keys:
+            assert from_numpy((2,),
+                              np.dtype('M8[%s]' % k)) == dshape('2 * datetime')
+
+    def test_date(self):
+        for d in ('D', 'M', 'Y', 'W'):
+            assert from_numpy((2,),
+                              np.dtype('M8[%s]' % d)) == dshape('2 * date')
+
+    def test_ascii_string(self):
+        assert (from_numpy((2,), np.dtype('S7')) ==
+                dshape('2 * string[7, "ascii"]'))
+
+    def test_string(self):
+        assert from_numpy((2,), np.dtype('U7')) == dshape('2 * string[7]')
+
 
 class TestOther(unittest.TestCase):
     def test_eq(self):
