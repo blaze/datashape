f2a3afd60a7212c1c4fb449e627d6f6c83bff875
Matthew Rocklin <mrocklin@gmail.com>
2014-11-03 10:02:30 -0800

    remove install_requires from setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     url = "http://packages.python.org/datashape",
     packages = ["datashape",
                 "datashape.tests"],
-    install_requires=open('requirements.txt').read().split('\n'),
+    # install_requires=open('requirements.txt').read().split('\n'),
     long_description = read('README.rst'),
     classifiers = [
         "Development Status :: 3 - Alpha",
@@ -28,4 +28,3 @@ setup(
         "License :: OSI Approved :: BSD License",
     ],
 )
-
