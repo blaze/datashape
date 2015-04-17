805b6d3030760f925090a198c83d4cf90475a026
Phillip Cloud <cpcloud@gmail.com>
2015-04-17 10:59:40 -0400

    Versioneer in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 import os
 from distutils.core import setup
 
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = os.path.join('datashape', '_version.py')
+versioneer.versionfile_build = versioneer.versionfile_source
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'datashape-'  # dirname like 'myproject-1.2.0'
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -14,7 +21,8 @@ def read(fname):
 
 setup(
     name="datashape",
-    version="0.4.4",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="Continuum Analytics",
     author_email="blaze-dev@continuum.io",
     description="A data description language.",
