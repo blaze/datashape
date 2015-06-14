e0acb3e70d7d678507ae7a6ace469b58d1fd7746
Phillip Cloud <cpcloud@gmail.com>
2015-06-14 12:50:55 -0400

    Rename to jsonio

---
+++ datashape/jsonio.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+
+import numbers
+import json
+
+from itertools import chain
+
+import numpy as np
+
+import datashape
+from .dispatch import dispatch
+from .coretypes import Record, CType, DataShape, Var, Fixed, Option
+
+
+@dispatch(basestring)
+def to_json(s):
+    return s
+
+
+@dispatch(Record)
+def to_json(ds):
+    return dict(names=ds.names,
+                types=dict((name, to_json(typ)) for name, typ in ds.fields))
+
+
+@dispatch((Option, CType))
+def to_json(u):
+    return to_json(str(u))
+
+
+def jsonify(dshape):
+    return json.dumps(to_json(dshape))
+
+
+@dispatch(Var)
+def to_json(v):
+    return 'var'
+
+
+@dispatch(Fixed)
+def to_json(f):
+    return int(f)
+
+
+@dispatch(DataShape)
+def to_json(ds):
+    return dict(shape=list(map(to_json, ds.shape)),
+                measure=to_json(ds.measure))
+
+
+@dispatch(dict)
+def from_json(d):
+    if 'measure' in d and 'shape' in d:
+        # toplevel dshape
+        shape = tuple(from_json(dim) for dim in d['shape'])
+        measure = from_json(d['measure']),
+        return DataShape(*chain(shape, measure))
+    else:
+        # record dshape
+        types = d['types']
+        return Record([(f, from_json(types[f])) for f in d['names']])
+
+
+@dispatch(basestring)
+def from_json(s):
+    try:
+        return datashape.dshape(s).measure
+    except datashape.DataShapeSyntaxError:
+        return getattr(datashape, s, s)
+
+
+@dispatch(numbers.Number)
+def from_json(n):
+    return n
