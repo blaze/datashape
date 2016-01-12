d10468e6b5393b079622fa7ef1a404cef047a701
Joe Jevnik <joe@quantopian.com>
2016-01-11 19:45:06 -0500

    ENH: add timedelta handling to maxtype

--- datashape/typesets.py
+++ datashape/typesets.py
@@ -6,8 +6,7 @@ from itertools import chain
 
 from .coretypes import (Unit, int8, int16, int32, int64, uint8, uint16, uint32,
                         uint64, float16, float32, float64, complex64,
-                        complex128, bool_, Decimal)
-from .coretypes import Option
+                        complex128, bool_, Decimal, TimeDelta, Option)
 
 
 __all__ = ['TypeSet', 'matches_typeset', 'signed', 'unsigned', 'integral',
@@ -185,17 +184,25 @@ def maxtype(measure):
 
     >>> maxtype(Option(Decimal(11, 2)))
     Option(ty=Decimal(precision=11, scale=2))
+
+    >>> maxtype(TimeDelta(unit='ms'))
+    TimeDelta(unit='ms')
+
+    >>> maxtype(Option(TimeDelta(unit='ms')))
+    Option(ty=TimeDelta(unit='ms'))
     """
     measure = measure.measure
     isoption = isinstance(measure, Option)
     if isoption:
         measure = measure.ty
-    if not matches_typeset(measure, scalar) and not isinstance(measure, Decimal):
+    if (not matches_typeset(measure, scalar) and
+        not isinstance(measure, (Decimal, TimeDelta))):
+
         raise TypeError('measure must be numeric')
 
     if measure == bool_:
         result = bool_
-    elif isinstance(measure, Decimal):
+    elif isinstance(measure, (Decimal, TimeDelta)):
         result = measure
     else:
         result = max(supertype(measure).types, key=lambda x: x.itemsize)
