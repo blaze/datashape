44e448e1c7171bd1dc6761ffa9c7eb152e5de04a
Kurt Smith <kwmsmith@gmail.com>
2016-04-11 11:14:50 -0500

    Refactor promote to correctly handle `string` and `?string`.

--- datashape/tests/test_promote.py
+++ datashape/tests/test_promote.py
@@ -1,5 +1,7 @@
-from datashape import promote, Option, float64, int64, float32, optionify
+import pytest
 
+from datashape import (promote, Option, float64, int64, float32, optionify,
+                       string, datetime_ as datetime)
 
 def test_simple():
     x = int64
@@ -27,3 +29,61 @@ def test_option_in_parent():
     y = Option(float32)
     z = optionify(x, y, y)
     assert z == y
+
+
+@pytest.mark.parametrize('x,y,p,r',
+                         [[string, string, True, string],
+                          [string, string, False, string],
+
+                          [Option(string),
+                           Option(string),
+                           True,
+                           Option(string)],
+
+                          [Option(string),
+                           Option(string),
+                           False,
+                           Option(string)],
+
+                          [Option(string),
+                           string,
+                           True,
+                           Option(string)],
+
+                          [Option(string),
+                           string,
+                           False,
+                           string]])
+def test_promote_string_with_option(x, y, p, r):
+    assert (promote(x, y, promote_option=p) ==
+            promote(y, x, promote_option=p) ==
+            r)
+
+
+@pytest.mark.parametrize('x,y,p,r',
+                         [[datetime, datetime, True, datetime],
+                          [datetime, datetime, False, datetime],
+
+                          [Option(datetime),
+                           Option(datetime),
+                           True,
+                           Option(datetime)],
+
+                          [Option(datetime),
+                           Option(datetime),
+                           False,
+                           Option(datetime)],
+
+                          [Option(datetime),
+                           datetime,
+                           True,
+                           Option(datetime)],
+
+                          [Option(datetime),
+                           datetime,
+                           False,
+                           datetime]])
+def test_promote_datetime_with_option(x, y, p, r):
+    assert (promote(x, y, promote_option=p) ==
+            promote(y, x, promote_option=p) ==
+            r)
