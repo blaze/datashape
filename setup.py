67ac656899eba02552c84510ff5929fd9d8971af
KOLANICH <kolan_n@mail.ru>
2023-02-14 14:01:10 +0300

    ENH: Move the most of metadata into `setup.cfg`, the declarative format supported since Dec 2015.

--- setup.py
+++ setup.py
@@ -1,42 +1,9 @@
-import os
 from setuptools import setup
 
 import versioneer
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
-def read(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
-        return f.read()
-
-
-setup(
-    name='datashape',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author='Continuum Analytics',
-    author_email='blaze-dev@continuum.io',
-    description='A data description language.',
-    license='BSD-2-Clause',
-    keywords='data language',
-    url='https://github.com/blaze/datashape',
-    project_urls = {
-        "Source Code": "https://github.com/blaze/datashape",
-        "Bug Tracker": "https://github.com/blaze/datashape/issues",
-        "Documentation": "https://datashape.readthedocs.io/en/latest/",
-    },
-    packages=['datashape', 'datashape.util', 'datashape.tests'],
-    install_requires=read('requirements.txt').strip().split('\n'),
-    long_description=read('README.rst'),
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'License :: OSI Approved :: BSD License',
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+    )
